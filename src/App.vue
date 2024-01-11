68cd892bd57ae0bebb026aac6b517650fc444081
imLukePex <luca.pessione@gmail.com>
2024-01-11 16:46:30 +0100

    added chiamata axios

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <script>
+// Import Axios
+import axios from 'axios';
+
+// Import Store.js
+import { store } from './store';
+
+// Import componenti figli
 import AppHeader from './components/AppHeader.vue';
 import MoviesList from './components/MoviesList.vue';
 
@@ -6,6 +13,28 @@ export default {
     components: {
         AppHeader,
         MoviesList
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+        getMovies() {
+            // Chiamata Movies
+            axios
+                .get(store.apiMovies)
+                .then((res => {
+                    console.log(res.data.results);
+                    store.moviesList = res.data.results;
+                }))
+                .catch((err) => {
+                    console.log("Errori", err);
+                });
+        }
+    },
+    created() {
+        this.getMovies();
     }
 }
 </script>
